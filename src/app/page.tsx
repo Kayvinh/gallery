bfe15823413d4bbba6e330984451e1d2933d8f5f
Kevin Nguyen <kayvinhnguyen@gmail.com>
2024-04-17 18:03:55 -0700

    add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { db } from "~/server/db";
 
+
 export const dynamic = "force-dynamic";
+
+async function DisplayImages() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, idx) => (
+        <div key={image.id + '-' + idx} className="w-48">
+          <Image className="h-72 w-96" src={image.url} alt="image" width={500} height={400} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 
 export default async function HomePage() {
@@ -13,13 +32,12 @@ export default async function HomePage() {
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, idx) => (
-          <div key={image.id + '-' + idx} className="w-48">
-            <Image className="h-72 w-96" src={image.url} alt="image" width={500} height={400} />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above.</div>
+      </SignedOut>
+      <SignedIn>
+        <DisplayImages />
+      </SignedIn>
     </main>
   );
 }
